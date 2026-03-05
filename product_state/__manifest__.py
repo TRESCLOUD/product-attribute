8f2af72c453d98226f07c751bb8379de13e0b625
Cesar J. Santacruz <cesarjsantacruz@gmail.com>
2026-03-05 13:17:59 -0500

    [IMP][CS] Update length

--- product_state/__manifest__.py
+++ product_state/__manifest__.py
@@ -18,7 +18,9 @@
         "views/product_state_views.xml",
     ],
     "application": False,
-    "auto_install": True, # TRESCLOUD: Util para instalación automática desde el autoinstaller. Módulo: Product Tier Validation (product_tier_validation)
+    # TRESCLOUD: Util para instalación automática desde el autoinstaller.
+    # Módulo: Product Tier Validation (product_tier_validation)
+    "auto_install": True,
     "maintainers": ["emagdalenaC2i"],
     "post_init_hook": "post_init_hook",
 }
